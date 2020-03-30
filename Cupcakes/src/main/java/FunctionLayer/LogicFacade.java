1c80cbf7569d1beff6ad295333a677900750a073
Etlos01 <etlos01@gmail.com>
2020-03-30 21:41:47 +0200

    Vi prøver igen

--- Cupcakes/src/main/java/FunctionLayer/LogicFacade.java
+++ Cupcakes/src/main/java/FunctionLayer/LogicFacade.java
@@ -6,8 +6,6 @@ import DBAccess.UserMapper;
 
 import java.util.ArrayList;
 
-import java.util.ArrayList;
-
 /**
  * The purpose of LogicFacade is to...
  * @author kasper
@@ -16,11 +14,34 @@ public class LogicFacade {
 
     public static User login( String email, String password ) throws LoginSampleException {
         return UserMapper.login( email, password );
-    } 
+    }
 
     public static User createUser( String email, String password ) throws LoginSampleException {
         User user = new User(email, password, "customer");
         UserMapper.createUser( user );
         return user;
     }
+    public static ArrayList<Bottom> getBottoms() throws LoginSampleException {
+        return CupcakeMapper.getBottoms();
+    }
+
+    public static ArrayList<Topping> getToppings() throws LoginSampleException {
+        return CupcakeMapper.getToppings();
+    }
+
+    public static int createOrder(int userId) throws LoginSampleException {
+        return OrderMapper.createOrder(userId);
+    }
+    public static void createOrderLine(int orderId, int toppingId, int bottomId, int quantity, int sum) throws LoginSampleException {
+        OrderMapper.createOrderLine(orderId, toppingId, bottomId, quantity, sum);
+    }
+    public static void updateUserCredit(int userId, double totalPrice) throws LoginSampleException {
+        OrderMapper.updateUserCredit(userId,totalPrice);
+    }
+    public static ArrayList<OrderLine> getAllOrders() throws LoginSampleException {
+        return OrderMapper.getAllOrders();
+    }
+
+
+
 }
